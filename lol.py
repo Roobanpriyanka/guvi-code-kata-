fb18d0124f24ab577f52efe5c99bbec39a91938f
Roobanpriyanka <40803851+Roobanpriyanka@users.noreply.github.com>
2018-07-04 23:33:52 +0530

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,12 @@
+a=int(raw_input("enter the number a"))
+b=int(raw_input("enter the number b"))
+c=int(raw_input("enter the number c"))
+if ((a>b) and (a>c)):
+    print("a is greater")
+
+elif (b>c):
+     print("b is greater")
+
+else:
+    print("c is greater")
+     
