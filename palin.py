80e5314328f732c53f69bc775c5ea51e070e767b
Roobanpriyanka <40803851+Roobanpriyanka@users.noreply.github.com>
2018-07-09 21:09:46 +0530

    Add files via upload

---
+++ palin.py
@@ -0,0 +1,13 @@
+x=int(input("enter the number:"))
+temp=x
+rev=0
+while(x>0):
+
+    dig=x%10
+    rev=rev*10+dig
+    x=x//10
+
+if(temp==rev):
+    print("the number is palindrome:")
+else:
+    print("the number is not a palindrome:")
